eb80e7ca8a6030a667df1d675239596c1ad90b8b
Hu Yang <hlhyly950117@gmail.com>
2020-11-13 21:49:34 +0800

    [Edit] Refactor app.js in OOP approach. Add README file.

---
+++ README.md
@@ -0,0 +1,2 @@
+## Practice OOP using JavaScript
+- Build front end for a simple shopping cart web application.
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,44 +1,64 @@
-const products = [
-  {
-    title: "A Pillow",
-    imageURL:
-      "https://www.maxpixels.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg",
-    description: "A soft pillow!",
-    price: 19.99,
-  },
-  {
-    title: "A Carpet",
-    imageURL:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg",
-    description: "A carpet which you might like - or not.",
-    price: 89.99,
-  },
-];
+class Product {
+  constructor(title, imageURL, description, price) {
+    this.title = title;
+    this.imageURL = imageURL;
+    this.description = description;
+    this.price = price;
+  }
+}
 
-const productList = {
-  products,
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  render() {
+    const productElement = document.createElement("li");
+    productElement.className = "product-item";
+    productElement.innerHTML = `
+        <div>
+            <img src=${this.product.imageURL} alt=${this.product.description} />
+            <div class="product-item__content">
+                <h2>${this.product.title}</h2>
+                <h3>\$${this.product.price}</h3>
+                <p>${this.product.description}</p>
+                <button>Add to Cart</button>
+            </div>
+        </div>
+      `;
+    return productElement;
+  }
+}
+
+class ProductList {
+  products = [
+    new Product(
+      "A Pillow",
+      "https://www.maxpixels.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg",
+      "A soft pillow!",
+      19.99
+    ),
+    new Product(
+      "A Carpet",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg",
+      "A carpet which you might like - or not.",
+      89.99
+    ),
+  ];
+
   render() {
     const renderHook = document.getElementById("app");
     const productListSection = document.createElement("ul");
     productListSection.className = "product-list";
     for (const product of this.products) {
-      const productElement = document.createElement("li");
-      productElement.className = "product-item";
-      productElement.innerHTML = `
-        <div>
-            <img src=${product.imageURL} alt=${product.description} />
-            <div class="product-item__content">
-                <h2>${product.title}</h2>
-                <h3>\$${product.price}</h3>
-                <p>${product.description}</p>
-                <button>Add to Cart</button>
-            </div>
-        </div>
-      `;
+      const productItem = new ProductItem(product);
+      const productElement = productItem.render();
       productListSection.append(productElement);
     }
     renderHook.append(productListSection);
-  },
-};
+  }
+}
 
-productList.render(); 
+const productList = new ProductList();
+
+productList.render();
